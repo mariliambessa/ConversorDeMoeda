0ef53455ead86dd3721d25d8bb9a5d6cf2fe9596
Marilia Bessa <mariliambessa@gmail.com>
2019-07-08 20:45:19 -0300

    estrutura do app de converter

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// jQuery
+$(document).ready(function(){
+
+	const api = 'https://currencydatafeed.com/api/data.php?token=l5z9y4owzt3w2urzbfq3&currency=USD/BRL';
+	const botao = $('button');
+	let resultado = $('#resultado');
+
+	botao.click(function(){
+		let valor = $('#value').val();
+		
+		$.getJSON(api, function(data){
+			let val = data.currency[0].value;
+			let calculo = val*valor
+			resultado.html(`<p>${calculo.toFixed(2)}</p>`);
+		});
+	});
+
+});
+// end jQuery
