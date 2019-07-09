9457ddb24d3fbfb8f4622427d5720d0bbc6830b5
Marilia Bessa <mariliambessa@gmail.com>
2019-07-08 21:32:30 -0300

    adição de opção de moeda

--- js/main.js
+++ js/main.js
@@ -4,16 +4,21 @@ $(document).ready(function(){
 	const api = 'https://currencydatafeed.com/api/data.php?token=l5z9y4owzt3w2urzbfq3&currency=EUR/BRL';
 	const botao = $('button');
 	let resultado = $('#resultado');
+	const select = $('select');
 
 	botao.click(function(){
 		let valor = $('#value').val();
 		
 		$.getJSON(api, function(data){
 			let val = data.currency[0].value;
-			let calculo = val*valor
+			let calculo = val*valor;
 			resultado.html(`<p>${calculo.toFixed(2)}</p>`);
 			$('#value').val(1).focus();
 		});
+	});
+
+	select.change(function(){
+		console.log(this.value);
 	});
 
 });
