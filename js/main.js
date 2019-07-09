02df4ead90dd04b7760bd9534fd3908fe8592ec1
Marilia Bessa <mariliambessa@gmail.com>
2019-07-08 21:45:28 -0300

    mudança de moeda

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
 // jQuery
 $(document).ready(function(){
 
-	const api = 'https://currencydatafeed.com/api/data.php?token=l5z9y4owzt3w2urzbfq3&currency=EUR/BRL';
-	const botao = $('button');
 	let resultado = $('#resultado');
 	const select = $('select');
 
-	botao.click(function(){
+	select.change(function(){
+		let moeda = this.value;
+		const api = `https://currencydatafeed.com/api/data.php?token=l5z9y4owzt3w2urzbfq3&currency=${moeda}/BRL`; 
+			
 		let valor = $('#value').val();
-		
 		$.getJSON(api, function(data){
 			let val = data.currency[0].value;
 			let calculo = val*valor;
 			resultado.html(`<p>${calculo.toFixed(2)}</p>`);
+			
 			$('#value').val(1).focus();
 		});
-	});
 
-	select.change(function(){
-		console.log(this.value);
+		console.log(this.value); //o value de uma const pode ser trocado
 	});
 
 });
